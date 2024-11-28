aaebc1a41a4089ed019e46fe9e4f85ed987a1e39
VisualStudioUser273 <arthurep123@gmail.com>
2024-11-28 17:30:54 +0000

    Add files via upload

---
+++ Rocket Game.py
@@ -0,0 +1,67 @@
+import pygame
+from pygame.locals import*
+from time import*
+
+
+pygame.init()
+screen=pygame.display.set_mode((600,600))
+player=pygame.image.load("Rocket.png")
+bg=pygame.image.load("Bakground.png")
+
+playing=True
+
+player_x = 200
+
+player_y = 200
+
+
+while playing:
+    screen.blit(bg,(0,0))
+    screen.blit(player,(player_x,player_y))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_UP:
+                player_y=player_y-5
+
+
+###
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_DOWN:
+                player_y=player_y+5
+
+###
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_LEFT:
+                player_x=player_x-5    
+
+
+### 
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+        if event.type == pygame.KEYDOWN:
+            if event.key==K_RIGHT:
+                player_=player_x+5    
+
+    
+      
+
+
+    pygame.display.update()
+
+

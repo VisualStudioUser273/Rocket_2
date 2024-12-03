31f2f9f012b5ccc6360e3d0f2b374c1b1632e93a
VisualStudioUser273 <arthurep123@gmail.com>
2024-12-03 18:21:26 +0000

    Update Rocket Game.py

--- Rocket Game.py
+++ Rocket Game.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 from pygame.locals import*
 from time import*
 
@@ -19,49 +20,40 @@ while playing:
     screen.blit(bg,(0,0))
     screen.blit(player,(player_x,player_y))
 
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit(0)
         if event.type == pygame.KEYDOWN:
             if event.key==K_UP:
-                player_y=player_y-5
+                player_y=player_y-25
 
 
 ###
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
+           
             if event.key==K_DOWN:
                 player_y=player_y+5
 
 ###
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
+        
             if event.key==K_LEFT:
                 player_x=player_x-5    
 
 
-### 
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit(0)
-        if event.type == pygame.KEYDOWN:
+###
+       
             if event.key==K_RIGHT:
-                player_=player_x+5    
+                player_x=player_x+5 
 
+    if player_y==600:
+        pygame.quit()        
+
+
+
+    player_y+=5
+    sleep(0.05)  
+
+            
     
       
-
-
-    pygame.display.update()
-
-
